0e5458399f240bd1b8dfba1290db6974b353f0d5
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-18 09:18:21 -0300

    feito: maipulação de arquivos

---
+++ file.js
@@ -0,0 +1,16 @@
+// node file.js
+const fs = require('fs');
+
+// fs.writeFile('hello.txt', 'Hello World!', err => {
+//   console.log(err);
+// })
+
+// fs.rename('test.txt', 'test2.txt', err => {
+//   console.log(err);
+// })
+
+// fs.unlink('test2.txt', err => {
+//   console.log(err);
+// })
+
+console.log(__dirname);
